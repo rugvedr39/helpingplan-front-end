8b9c51d64e46bd490dc9b009d555be5f02d2a94e
Rugved-Technify <tech01.twtspl@gmail.com>
2024-05-11 11:23:38 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'helping_plan',
+  webDir: 'www'
+};
+
+export default config;
